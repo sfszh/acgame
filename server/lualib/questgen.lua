6dd68ffabb4ecf687a7c59cfa174b5548a7833cb
sfszh <sophoszhang@gmail.com>
2016-04-04 08:10:40 +0000

    add some more quest

--- server/lualib/questgen.lua
+++ server/lualib/questgen.lua
@@ -2,118 +2,151 @@ local M= {}
 
 
 M.actions = {
-  ['goto'] ={ 
-    {
-      description = 'Just wander around and look',
-      sequence = {'>explore'}
+    ['goto'] ={ 
+        {
+            description = 'Just wander around and look',
+            sequence = {'>explore'}
+        }
+    },
+    ['learn'] ={
+        {
+            description = 'Go someplace, get something, and read what is written on it',
+            sequence = {'goto', 'get', '>read'}
+        }
+    },
+    ['get'] = {
+        {
+            description = 'steal it from somebody',
+            sequence = {'steal'}
+        },
+        {
+            description = 'Go someplace and pick something up which lying around',
+            sequence = {'goto', '>gather'}
+        }
+    },
+    ['steal'] = {
+        {
+            description = 'Go someplcae, sneek up on somebody, and take something',
+            sequence = {'goto', '>stealth', '>take'}
+        }
+    },
+    ['spy'] = {
+        {
+            description = 'go someplcae, spy on somebody, return and report',
+            sequence = {'goto', 'kill', '>take'}
+        }
+    },
+    ['capture'] = {
+        {
+            description = 'Get something, go someplace and use it to capture somebody',
+            sequence = { 'get', 'goto', '>capture'}
+        }
+    },
+    ['kill'] = {
+        {
+            description = 'Go someplace and kill somebody',
+            sequence = {'goto', '>kill'}
+        }
     }
-  },
-  ['learn'] ={
-    {
-      description = 'Go someplcae, get something, and read what is written on it',
-      sequence = {'goto', 'get', '>read'}
-    }
-  },
-  ['get'] = {
-    {
-      description = 'steal it from somebody',
-      sequence = {'steal'}
-    }
-  },
-  ['steal'] = {
-    {
-      description = 'Go someplcae, sneek up on somebody, and take something',
-      sequence = {'goto', '>stealth', '>take'}
-    }
-  },
-  ['spy'] = {
-    {
-      description = 'go someplcae, spy on somebody, return and report',
-      sequence = {'goto', 'kill', '>take'}
-    }
-  },
-  ['capture'] = {
-    {
-      description = 'Get something, go someplace and use it to capture somebody',
-      sequence = { 'get', 'goto', '>capture'}
-    }
-  },
-  ['kill'] = {
-    {
-      description = 'Go someplace and kill somebody',
-      sequence = {'goto', '>kill'}
-    }
-  }
 }
 
 M.motivations = {
-  ['knowledge'] = {
-    description = 'Deliver item for study',
-    sequence = {'get', 'goto', '>give'}
-  }
+    ['knowledge'] = {
+        {
+            description = 'Deliver item for study',
+            sequence = {'get', 'goto', '>give'}
+        }
+    },
+    ['comfort'] = {
+        {   
+            description = 'Obtain luxuries', 
+            sequence = {'get', 'goto', '>give'}
+        },
+        {   
+            description = 'Kill pests',
+            sequence    = {'goto', '>damage', 'goto', '>report'}
+        },
+    },
+    ['reputation'] = {
+        {   
+            description ='Obtain rare items',
+            sequence    = {'get', 'goto', '>give'}
+        },
+        {   
+            description = 'Kill enemies',
+            sequence    = {'goto', 'kill', 'goto', '>report'}
+        },
+        {   
+            description = 'Visit a dangerous place',
+            sequence    = {'goto', 'goto', '>report'}
+        },
+    },
 }
 
 M.actions_index = {'goto', 'learn', 'get', 'steal', 'spy', 'capture', 'kill'}
 
 local function random_action()
 
-  local index = math.random(1, #M.actions_index)
-  return M.actions[M.actions_index[index]]
+    local index = math.random(1, #M.actions_index)
+    return M.actions[M.actions_index[index]]
 end
 
 M.generated_steps = {}
 M.depth = 0
 function M.generatequest(depth)
-  local motivation = M.random_from(M.motivations)
-  print("Generating a Quest for "..motivation.description .. ":" .. M.sequence_to_str(motivation.sequence))
-  M.generate_step(motivation.sequence)
-  for _, step in pairs(M.generated_steps) do
-    print(step)
-  end
+    local motivation = M.random_from(M.random_from(M.motivations))
+    print("Generating a Quest for "..motivation.description .. ":" .. M.sequence_to_str(motivation.sequence))
+    M.generate_step(motivation.sequence)
+    for _, step in pairs(M.generated_steps) do
+        print(step)
+    end
 end
 
 function M.random_from( t )
-  local choice = "F"
-  local n = 0
-  for i, o in pairs(t) do 
-    n = n + 1
-    if math.random() < (1/n) then
-      choice = o
+    local choice = "F"
+    local n = 0
+    for i, o in pairs(t) do 
+        n = n + 1
+        if math.random() < (1/n) then
+            choice = o
+        end
     end
-  end
-  return choice
+    return choice
 end
 
 function M.generate_step(sequence)
-  for _,action in pairs(sequence) do
-    if string.match(action, '>.-$') == nil then
-      local step = M.random_from( M.actions[action])
-      M.generated_steps[#M.generated_steps + 1] = M.padding_by(M.depth)..step.description .. ":" .. M.sequence_to_str(step.sequence)
-      M.depth = M.depth + 1
-      M.generate_step(step.sequence)
-    else 
-      M.depth = M.depth - 1
-      M.generated_steps[#M.generated_steps + 1] = M.padding_by(M.depth).. action
-      return
-      -- print(action)
+    for _,action in pairs(sequence) do
+        -- print("action is "..action)
+        if string.match(action, '>.-$') == nil then
+            local step = M.random_from( M.actions[action])
+            M.generated_steps[#M.generated_steps + 1] = M.padding_by(M.depth)..step.description .. ":" .. M.sequence_to_str(step.sequence)
+            M.depth = M.depth + 1
+            -- print("match")
+            M.generate_step(step.sequence)
+        else 
+
+            M.depth = M.depth - 1
+            M.generated_steps[#M.generated_steps + 1] = M.padding_by(M.depth).. action
+            return
+            -- print(action)
+        end
     end
-  end
 end
 
 function M.sequence_to_str(sequence)
-  local str = ""
-  for _,v in pairs(sequence) do
-    str = str .. v ..' '
-  end
-  return '['..str..']'
+    local str = ""
+    for _,v in pairs(sequence) do
+        str = str .. v ..' '
+    end
+    return '['..str..']'
 end
 
 function M.padding_by( numbers )
-  padding = "  "
-  for i = 1, numbers, 1 do 
-    padding = padding .. padding
-  end
-  return padding
+    padding = "  "
+    for i = 1, numbers, 1 do 
+        padding = padding .. padding
+    end
+    return padding
 end
 
 return M
